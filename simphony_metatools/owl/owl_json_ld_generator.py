0f4779f0b4393daea1eeeb92c8fdf09ae6111eb3
Stefano Borini <sborini@enthought.com>
2017-03-21 10:50:35 +0000

    Basic OWL generator completion

--- simphony_metatools/owl/owl_json_ld_generator.py
+++ simphony_metatools/owl/owl_json_ld_generator.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from simphony_metaparser.nodes import VariableProperty
 from simphony_metaparser.utils import traverse, without_cuba_prefix
@@ -11,10 +12,21 @@ RDF_SCHEMA_PREFIX = "http://www.w3.org/2000/01/rdf-schema#"
 XMLSCHEMA_PREFIX = "http://www.w3.org/2001/XMLSchema#"
 
 
-class OWLJSONLDGenerator():
+class OWLJSONLDGenerator(object):
     """Creates a OWL representation of our data model in JSON-LD.
     """
     def generate(self, ontology, output_path, overwrite=False):
+
+        if os.path.exists(output_path):
+            if overwrite:
+                try:
+                    os.remove(output_path)
+                except OSError:
+                    pass
+            else:
+                raise OSError('Generated files already present. '
+                              'Will not overwrite without --overwrite')
+
         onto = [
             {
                 "@id": ONTOLOGY_PREFIX,
@@ -39,34 +51,36 @@ class OWLJSONLDGenerator():
                 if prop.name == "definition":
                     entry[RDF_SCHEMA_PREFIX+"comment"] = prop.default
 
-
             onto.append(entry)
+
+        datatype_properties, object_properties = \
+            self._extract_properties(ontology)
+
+        datatype_domain_range = {}
+        for s, p, o in datatype_properties:
+            datatype_domain_range.setdefault(p, []).append((s, o))
 
         for datatype in ontology.data_types:
             entry = {
                 "@id": ONTOLOGY_PREFIX+without_cuba_prefix(datatype.name),
                 "@type": OWL_PREFIX+"DatatypeProperty",
-                RDF_SCHEMA_PREFIX+"range": [
-                    {
-                        "@id": XMLSCHEMA_PREFIX+str(datatype.type)
-                    }
-                ],
                 RDF_SCHEMA_PREFIX+"comment": [{
                     "@value": datatype.definition
                 }],
-
             }
+
+            entry.setdefault(RDF_SCHEMA_PREFIX+"range", []).append(
+                {
+                    "@id": XMLSCHEMA_PREFIX+datatype.type
+                }
+            )
+
             onto.append(entry)
 
-        for s, p, o in self.extract_object_properties(ontology):
+        for s, p, o in object_properties:
             entry = {
                 "@id": ONTOLOGY_PREFIX+p,
                 "@type": OWL_PREFIX+"ObjectProperty",
-                RDF_SCHEMA_PREFIX+"domain": [
-                     {
-                        "@id": ONTOLOGY_PREFIX+s
-                    }
-                  ],
                 RDF_SCHEMA_PREFIX+"range": [
                     {
                         "@id": ONTOLOGY_PREFIX+o
@@ -78,18 +92,38 @@ class OWLJSONLDGenerator():
         with open(output_path, "wb") as f:
             json.dump(onto, f, indent=4)
 
-    def extract_object_properties(self, ontology):
-        """Extracts the object properties, skipping the datatype properties,
-        from our ontology"""
-        data_types = [datatype.name for datatype in ontology.data_types]
+    def _extract_properties(self, ontology):
+        """Extracts the datatype and object properties from our ontology
+
+        Parameters
+        ----------
+        ontology : Ontology
+            The ontology
+
+        Returns
+        -------
+        datatype_properties, object_properties
+            The properties, each as a list of triples subject property object.
+        """
+        data_types = {datatype.name: datatype
+                      for datatype in ontology.data_types}
+
+        datatype_properties = []
         object_properties = []
+
         for item, _ in traverse(ontology.root_cuds_item):
             for prop in [p for p in item.properties.values()
                          if isinstance(p, VariableProperty)]:
-                if prop.name not in data_types:
+                if prop.name in data_types:
+                    datatype_properties.append(
+                        (without_cuba_prefix(item.name),
+                         without_cuba_prefix(prop.name),
+                         data_types[prop.name].type)
+                    )
+                else:
                     object_properties.append(
                         (without_cuba_prefix(item.name),
                          without_cuba_prefix(prop.name),
                          without_cuba_prefix(prop.name)))
 
-        return object_properties
+        return datatype_properties, object_properties
